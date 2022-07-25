bf34f336ddb6f5181e903b5c603530c9cb4d2c4c
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-25 17:18:38 +0200

    add style main

--- index.php
+++ index.php
@@ -87,12 +87,26 @@ var_dump($faq);
                     </li>
                 </ul>
             </nav>
-
         </div>
         
     </header>
     <main>
-        Main
+        <div class="container">
+            <div class="faq">
+                <h2>
+                    Question
+                </h2>
+
+                <div>
+                    <p>
+                    La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+                    </p>
+                    <p>
+                    Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
+                    </p>
+                </div>
+            </div>
+        </div>
     </main>
     
 </body>
